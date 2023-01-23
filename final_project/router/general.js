7c7dc5256596394506443ff4c89465780dbbd78b
karanja <karanjavictor389@gmail.com>
2023-01-22 20:09:12 -0500

    updating the expressbookreview project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,11 +5,22 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const username = req.body.username;
+  const password = req.body.password;
 
+  if(username && password){
+      if(!isValid(username)){
+          users.push({"username": username, "password": password})
+          return res.status(200).json({message: "User successfully registered. Now you can log in"});
+      }else {
+          return res.status(404).json({message : "User already exists!"})
+      }
+  }
+  return res.status(404).json({message: "Unable to register user!"});
+});
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
@@ -28,26 +39,45 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+    const bookKeys = Object.keys(books)
     const author = req.params.author
-    for (let i=0; i<books.length; i++){
-    const keys = Object.keys(books[i])
-    if (author) {
-    res.send(books[author.keys])
+    for(let i = 0; i < bookKeys.length; i++) {
+        if(books[bookKeys[i]].author === author){
+            res.send({
+                author: books[bookKeys[i]].author,
+                title: books[bookKeys[i]].title,
+                reviews: books[bookKeys[i]].reviews
+            })
+        }
     }
-}
-  
+    
+   
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
+  const bookKeys = Object.keys(books)
+  const title = req.params.title
+  for(let i = 0; i < bookKeys.length; i++) {
+      if(books[bookKeys[i]].title === title){
+          res.send({
+              author: books[bookKeys[i]].author,
+              title: books[bookKeys[i]].title,
+              reviews: books[bookKeys[i]].reviews
+          })
+      }
+  }
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+  const Isbn = req.params.isbn
+  res.send(books[Isbn])
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
